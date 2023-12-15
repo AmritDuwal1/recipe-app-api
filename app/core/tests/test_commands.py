e10cffd2c0fd8679d96e41fb57092e51e687c1e6
AmritDuwal1 <61304552+AmritDuwal1@users.noreply.github.com>
2023-12-15 17:10:04 +0545

    added while loop

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,8 +9,8 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
-from app.core import tests
-import sys
+# from app.core import tests
+# import sys
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
@@ -22,7 +22,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db(self, patched_sleep, patched_check):
@@ -33,4 +33,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patch_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
